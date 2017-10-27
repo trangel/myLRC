7142b93355b87b1a8ad76f9e2d23a1c4303682bf
Tonatiuh Rangel <trangel@lbl.gov>
2017-10-27 11:52:18 -0700

    input variables for setup script

--- build/lib/myLRC/jobs_by_task.py
+++ build/lib/myLRC/jobs_by_task.py
@@ -1,13 +1,31 @@
 import os 
 
-def mkjob_by_task(jobname,dirname,cluster,nproc,modules):
-    #jobname='half-k{0}'.format(icalc+1)
-    #mkjob_by_task(jobname=jobname,
-    #    dirname=dirname,
-    #    cluster='nokomis',
-    #    nproc = flow.nproc,
-    #    modules = modules
-    
+def mkjob_by_task(**kwargs):
+    """
+    Writes a submission script for lawrencium clusters
+    --------------------------------------------------
+
+    Optional arguments:
+    jobname : str, default "job"
+    dirname : str, default './'
+    cluster : str, default 'nokomis'
+    nproc   : int, default 16
+    modules : str, default ''
+    """
+    jobname = kwargs.pop('jobname','job')
+    dirname = kwargs.pop('dirname','./')
+    cluster = kwargs.pop('cluster','nokomis')
+    nproc   = kwargs.pop('nproc',16)
+    modules = kwargs.pop('modules','')
+    #
+    # Check input parameters
+    #
+    known_clusters=['catamount','nokomis','minnehaha']
+    if cluster not in known_clusters:
+        print('Unknown cluster {}'.format(cluster))
+        print('Known clusters {}'.format(' '.join(known_clusters)))
+        exit(1)
+
     # Write taskfile1:
     newfile = os.path.join(dirname,"taskfile1")
     f = open (newfile,"w")
@@ -40,7 +58,7 @@ def mkjob_by_task(jobname,dirname,cluster,nproc,modules):
     f.write("cd 03-RPMNS\n")
     f.write("bash run.sh\n")
     f.write("cd ..\n")
-    f.write("cd 04-RPMNS\n")
+    f.write("cd 04-RESP\n")
     f.write("bash run.sh\n")
     f.write("cd ..\n")
     f.close()
@@ -49,19 +67,26 @@ def mkjob_by_task(jobname,dirname,cluster,nproc,modules):
     newfile = os.path.join(dirname,"job.sh")
     f = open(newfile,"w")
     f.write("#!/bin/bash -l\n")
-    f.write("#SBATCH  --job-name=SHG\n")
-    f.write("#SBATCH  --qos=condo_minnehaha\n")
-    f.write("#SBATCH  --account=lr_minnehaha\n")
-    f.write("#SBATCH  --partition=lr4\n")
-    f.write("#SBATCH  --ntasks=1\n")
+    f.write("#SBATCH  --job-name={}\n".format(jobname))
+    if dirname == 'minnehaha' :
+        f.write("#SBATCH  --qos=condo_minnehaha\n")
+        f.write("#SBATCH  --account=lr_minnehaha\n")
+        f.write("#SBATCH  --partition=lr4\n")
+    elif dirname == 'nokomis' :
+        f.write("#SBATCH  --qos=condo_nokomis\n")
+        f.write("#SBATCH  --account=lr_nokomis\n")
+        f.write("#SBATCH  --partition=lr3\n")
+    elif dirname == 'catamount' :
+        f.write("#SBATCH  --qos=cm_medium\n")
+        f.write("#SBATCH  --account=catamount\n")
+        f.write("#SBATCH  --partition=catamount\n")
+
+    f.write("#SBATCH  --ntasks={}\n".format(nproc))
     f.write("#SBATCH  -t 120:00:00\n")
     f.write("#SBATCH --mail-type=all\n")
     f.write("#SBATCH --mail-user=trangel@lbl.gov\n\n")
     f.write("bash /global/home/users/trangel/.optpy\n\n")
-    f.write("module swap gcc intel\n")
-    f.write("module load openmpi\n")
-    f.write("module load mkl\n")
-    f.write("module load intel/2013_sp1.4.211 openmpi hdf5/1.8.13-intel-p\n")
+    f.write(modules)
     f.write("\n\n")
     f.write("# Execution (ABINIT parallelized with MPI)\n")
     f.write("bash run1.sh\n\n")
